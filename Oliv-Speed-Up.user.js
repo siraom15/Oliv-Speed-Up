77a458c5f2c17b431c5d57a1997bcf7dd86a78a0
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-10-30 16:34:45 +0700

    Update : Fix bugs

--- Oliv-Speed-Up.user.js
+++ Oliv-Speed-Up.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Oliv-Speed-Up
-// @version      1.6
+// @version      1.7
 // @description  Oliv-Speed-Up
 // @author       siraom15
 // @match        https://learning.sit.kmutt.ac.th/
@@ -18,7 +18,7 @@
         whiteGray: "#f6f6f6",
         white: "white",
         black : "black"
-    }
+    };
 
     function createEl(elName, attributes) {
         let el = document.createElement(elName);
@@ -49,11 +49,11 @@
     function moveSlidePreview() {
         let point = document.querySelectorAll("vg-cuepoints");
         if (point != undefined) {
-            point[0].style.marginTop = "-1%"
-            clearInterval(moveSlidePreviewInterVal)
+            point[0].style.marginTop = "-1%";
+            clearInterval(moveSlidePreviewInterVal);
         }
     }
-    let moveSlidePreviewInterVal = setInterval(moveSlidePreview, 1000)
+    let moveSlidePreviewInterVal = setInterval(moveSlidePreview, 1000);
 
 
     function toggleSlidePreview() {
@@ -80,14 +80,7 @@
                 width : 100%;
                 text-align : center;
                 `
-            })
-            // let div1 = document.createElement('div');
-            // div1.style.marginTop = "1em";
-            // div1.style.padding = "1em";
-            // div1.style.color = "#626262"
-            // div1.style.backgroundColor = "#f6f6f6"
-            // div1.style.width = "100%";
-            // div1.style.textAlign = "center";
+            });
             let btn = createEl("button", {
                 style: `
                 text-align : right;
@@ -97,24 +90,14 @@
                 font-size : 1em;
                 `,
                 onclick: toggleSlidePreview
-            })
+            });
 
-            // var btn = document.createElement('button');
-            // btn.innerText = "Toggle Slide Preview"
-            // btn.addEventListener("click", toggleSlidePreview, false);
-            // btn.style.textAlign = "right"
-            // btn.style.color = "white"
-            // btn.style.backgroundColor = blue
-            // btn.style.border = "none"
-            // btn.style.fontSize = "1em";
-            appendManyChilds(div1, btn)
-            // div1.appendChild(btn)
+            appendManyChilds(div1, btn);
             container.parentNode.insertBefore(div1, container);
-
-            clearInterval(addBtnInterval)
+            clearInterval(addBtnInterval);
         }
     }
-    let addBtnInterval = setInterval(addBtn, 1000)
+    let addBtnInterval = setInterval(addBtn, 1000);
 
     function addDropDownSpeed() {
         let container = document.querySelector("div.video-info-container");
@@ -128,20 +111,11 @@
                 text-align : center;
                 `,
                 id: "setSpeedDiv",
-            })
-            // let div1 = document.createElement('div');
-            // div1.style.padding = "1em";
-            // div1.style.color = "#626262"
-            // div1.style.backgroundColor = "#f6f6f6"
-            // div1.style.width = "100%";
-            // div1.style.textAlign = "center";
-
-            // div1.id = "setSpeedDiv"
+            });
+            
             let label1 = createEl("label", {
                 innerText: "Speed : &nbsp;"
-            })
-            // let label1 = document.createElement('label');
-            // label1.innerHTML = "Speed : &nbsp;"
+            });
 
             let selectMenu = createEl('select', {
                 name: "setSpeed",
@@ -153,92 +127,54 @@
                 border : none;
                 `,
             });
-            // let selectMenu = document.createElement('select');
-            // selectMenu.name = "setSpeed"
-            // selectMenu.id = "setSpeed"
-            // selectMenu.addEventListener("change", changeSpeed, false);
-            // selectMenu.style.color = "white"
-            // selectMenu.style.backgroundColor = blue
-            // selectMenu.style.border = "none"
+           
             let option0 = createEl('option', {
                 value: 0.5,
                 innerText: "x0.5"
             });
-            // let option0 = document.createElement('option');
-            // option0.value = 0.5
-            // option0.innerHTML = "x0.5"
-
+           
             let option1 = createEl('option', {
                 value: 1,
                 innerText: "x1",
                 selected: 'selected'
             });
-            // let option1 = document.createElement('option');
-            // option1.value = 1
-            // option1.innerHTML = "x1"
-            // option1.setAttribute('selected', 'selected');
-
+            
             let option2 = createEl('option', {
                 value: 1.25,
                 innerText: "x1.25",
             });
-            // let option5 = document.createElement('option');
-            // option5.value = 1.25
-            // option5.innerHTML = "x1.25"
-
+           
             let option3 = createEl('option', {
                 value: 1.5,
                 innerText: "x1.5",
             });
-            // let option2 = document.createElement('option');
-            // option2.value = 1.5
-            // option2.innerHTML = "x1.5"
-
+           
             let option4 = createEl('option', {
                 value: 2,
                 innerText: "x2",
             });
-            // let option3 = document.createElement('option');
-            // option3.value = 2
-            // option3.innerHTML = "x2"
-
+           
             let option5 = createEl('option', {
                 value: 5,
                 innerText: "x5",
             });
-            // let option4 = document.createElement('option');
-            // option4.value = 5
-            // option4.innerHTML = "x5"
 
             let option6 = createEl('option', {
                 value: 16,
                 innerText: "x16",
             });
-            // let option6 = document.createElement('option');
-            // option6.value = 16
-            // option6.innerHTML = "x16 Max Speed"
-
 
             let options = [option0, option1, option2, option3, option4, option5,option6];
-            appendManyChilds(selectMenu, ...options)
-            // selectMenu.appendChild(option0);
-            // selectMenu.appendChild(option1);
-            // selectMenu.appendChild(option5);
-            // selectMenu.appendChild(option2);
-            // selectMenu.appendChild(option3);
-            // selectMenu.appendChild(option4);
-            // selectMenu.appendChild(option6);
-
+            appendManyChilds(selectMenu, ...options);
+            
             appendManyChilds(div1, label1, selectMenu);
-            // div1.appendChild(label1)
-            // div1.appendChild(selectMenu)
-
+            
             container.parentNode.insertBefore(div1, container);
-
-            clearInterval(addDropDownSpeedInterval)
+            
+            clearInterval(addDropDownSpeedInterval);
         }
     }
-    let addDropDownSpeedInterval = setInterval(addDropDownSpeed, 1000)
+    let addDropDownSpeedInterval = setInterval(addDropDownSpeed, 1000);
 
     function addSkipTime() {
         let container = document.querySelector("div.video-info-container");
@@ -253,19 +189,10 @@
                 `,
                 id: "skipTime"
             });
-            // let div1 = document.createElement('div');
-            // div1.style.padding = "1em";
-            // div1.style.color = "#626262"
-            // div1.style.backgroundColor = "#f6f6f6"
-            // div1.style.width = "100%";
-            // div1.style.textAlign = "center";
-            // div1.id = "skipTime"
-
+            
             let label1 = createEl('label', {
-                innerText = 'Set Skip time : &nbsp;'
+                innerText : 'Set Skip time : &nbsp;'
             });
-            // let label1 = document.createElement('label');
-            // label1.innerHTML = "Set Skip time : &nbsp;"
 
             let selectMenu = createEl('select', {
                 name: "setSkipTime",
@@ -277,118 +204,61 @@
                 border : none;
                 `,
             });
-            // let selectMenu = document.createElement('select');
-            // selectMenu.name = "setSkipTime"
-            // selectMenu.id = "setSkipTime"
-            // selectMenu.addEventListener("change", changeSkipTime, false);
-            // selectMenu.style.color = "white"
-            // selectMenu.style.backgroundColor = blue
-            // selectMenu.style.border = "none"
-
+           
             let option0 = createEl('option', {
                 value: 1,
                 innerText: "1 sec"
             });
-            // let option0 = document.createElement('option');
-            // option0.value = 1
-            // option0.innerHTML = "1 sec"
-
+          
             let option1 = createEl('option', {
                 value: 2,
                 innerText: "2 sec"
             });
-            // let option1 = document.createElement('option');
-            // option1.value = 2
-            // option1.innerHTML = "2 sec"
-
+           
             let option2 = createEl('option', {
                 value: 3,
                 innerText: "3 sec"
             });
-            // let option5 = document.createElement('option');
-            // option5.value = 3
-            // option5.innerHTML = "3 sec"
-
+           
             let option3 = createEl('option', {
                 value: 5,
                 innerText: "5 sec",
                 selected: 'selected'
             });
-            // let option2 = document.createElement('option');
-            // option2.value = 5
-            // option2.innerHTML = "5 sec"
-            // option2.setAttribute('selected', 'selxected');
-
+           
             let option4 = createEl('option', {
                 value: 10,
                 innerText: "10 sec"
             });
-            // let option3 = document.createElement('option');
-            // option3.value = 10
-            // option3.innerHTML = "10 sec"
 
             let option5 = createEl('option', {
                 value: 10,
                 innerText: "10 sec"
             });
-            // let option4 = document.createElement('option');
-            // option4.value = 30
-            // option4.innerHTML = "30 sec"
 
             let options = [option0, option1, option2, option3, option4, option5];
-            appendManyChilds(selectMenu, ...options)
-            // selectMenu.appendChild(option0);
-            // selectMenu.appendChild(option1);
-            // selectMenu.appendChild(option5);
-            // selectMenu.appendChild(option2);
-            // selectMenu.appendChild(option3);
-            // selectMenu.appendChild(option4);
+            appendManyChilds(selectMenu, ...options);
 
             let small1 = createEl("small", {
                 innerText: '**Skip time when you press the arrow key**'
             });
-            // let small = document.createElement("small")
-            // small.innerHTML = "**Skip time when you press the arrow key**"
-
 
             let br = createEl("br")
             let hr = createEl("hr");
 
             let small2 = createEl("small", {
                 innerText: 'develop by '
-            })
+            });
+            
             let alink = createEl("a", {
                 href: 'https://github.com/siraom15/Oliv-Speed-Up',
                 target: '_blank',
                 innerText: "aommie"
-            })
-            // alink.href = "https://github.com/siraom15"
-            // alink.target = "_blank"
-            // alink.innerHTML = "áommie"
-            // sm.innerHTML = "develop by "
-
+            });
+            
             appendManyChilds(sm, alink)
-            // sm.appendChild(alink)
-
-
-            // let sm2 = document.createElement("small")
-            // let alink2 = document.createElement("a")
-            // alink2.href = "https://github.com/siraom15/Oliv-Speed-Up/raw/main/Oliv-Speed-Up.user.js"
-            // alink2.target = "_blank"
-            // alink2.innerHTML = " here"
-            // sm2.innerHTML = "Check for update"
-
-            // sm2.appendChild(alink2)
 
             appendManyChilds(div1, label1, selectMenu, br, small1, hr,small2)
-            // div1.appendChild(label1)
-            // div1.appendChild(selectMenu)
-            // div1.appendChild(br1)
-            // div1.appendChild(small)
-            // div1.appendChild(hr)
-            // div1.appendChild(sm)
-            // div1.appendChild(br)
-            // div1.appendChild(sm2)
 
             container.parentNode.insertBefore(div1, container);
 
@@ -421,11 +291,10 @@
                 transform: translate(-50%, -50%);
                 `,
                 innerText : text
-            })
-            // let div = document.createElement("div")
-            // div.setAttribute("style", "color: black;color: white;left: 50%;background-color: rgb(91, 144, 191);position: absolute;top: 0;padding: 2em;box-shadow: 0.1em 0.1em beige;left: 50%;margin-right: -50%;transform: translate(-50%, -50%);");
-            // div.innerHTML = text;
+            });
+
             container.parentNode.insertBefore(div, container);
+
             setTimeout(() => {
                 document.getElementById('toastBox').remove()
             }, 1000)
@@ -453,6 +322,7 @@
             skipTime(-(-skipTimeVal));
         }
         else if (e.keyCode == '32') {
+            //space bar
             togglePlay();
         }
     }
@@ -469,12 +339,11 @@
         let speedRate = document.getElementById("setSpeed").value;
         let vid = document.querySelector("video");
         vid.playbackRate = speedRate;
-        toastBox("changed speed to x" + speedRate);
+        toastBox(`changed speed to x ${speedRate}`);
     }
     function skipTime(time) {
         let vid = document.querySelector("video");
         vid.play()
         vid.currentTime += time;
-
     }
 })();
